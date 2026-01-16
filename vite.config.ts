c6361a4f08ee26288e16556f754db1990e4323d3
lilos <lilos8719@gmail.com>
2026-01-16 16:31:22 +0300

    Vercel web deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  server: {
-    port: 5173,
-    host: '0.0.0.0'
+    // ❌ Удалить publicDirectory
   }
 })
